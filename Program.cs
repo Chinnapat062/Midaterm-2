34d5789ef4f701c9740dd219bf593a923df698b4
Chinnapat062 <78645438+Chinnapat062@users.noreply.github.com>
2021-03-28 19:44:41 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ข้อ_2_กลางภาค
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int x=a,y=a;
+            Console.Write("input num a,b :")
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            while (x != y)
+            {
+                if(x<y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }    
+            }
+            Console.WriteLine(x);
+        }   
+    }
+}
